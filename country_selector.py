3f1341ca16e256af3b14586e8d84981c2d4c07cd
Leong Yen Ni <leongyenni@gmail.com>
2023-10-13 07:48:32 +0800

    Modified csv file

--- country_selector.py
+++ country_selector.py
@@ -9,11 +9,13 @@ countries_to_select = ['Japan', 'Switzerland', 'Malaysia', 'United States', 'Uni
 
 selected_countries = df[df['Country'].isin(countries_to_select)]
 
-columns_to_keep = ['Country', 'Year', 'Household air pollution', 'Ambient Particulate Matter Pollution', 'Total', 'Ambient Ozone Pollution']
+columns_to_keep = ['Country', 'Year', 'Household air pollution', 'Ambient Particulate Matter Pollution', 'Ambient Ozone Pollution']
 
 melted_df = pd.melt(selected_countries, id_vars=['Country', 'Year'], value_vars=columns_to_keep[2:], var_name='Air Pollution Type', value_name='Count')
 
 selected_year_df = melted_df[melted_df['Year'] == 2019]
+selected_year_df = selected_year_df.rename(columns={'Household air pollution': 'Household Air Pollution'})
+
 
 output_csv_file = './data/cleaned-death-rates-from-air-pollution.csv'
 
